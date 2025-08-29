c4108bff00de7b2a4d4c2a5897db7179e7800a0f
FernandoGuevaraD <fernandoguevara@udenar.edu.co>
2025-08-29 14:56:22 -0500

    Primer commit desde Android Studio

---
+++ app/src/main/java/com/example/myfirstproject/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myfirstproject
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Espera 4 segundos y abre MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Cierra el Splash para que no regrese al presionar "back"
+        }, 4000)
+    }
+}
